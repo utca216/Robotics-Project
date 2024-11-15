88d226d4da589d9fe411230da5a2e8d917f005e4
utca216 <151619685+utca216@users.noreply.github.com>
2024-11-15 08:28:16 +0600

    Update SumoRobotSketch.ino

--- SumoRobotSketch.ino
+++ SumoRobotSketch.ino
@@ -1,21 +1,37 @@
 #include <Ultrasonic.h>
+#include <Servo.h>
 
 // Пины для моторов и L298N
-#define IN1 2     //  Пины для управления направлением
+#define IN1 2
 #define IN2 3
 #define IN3 4
 #define IN4 5
-#define ENA 6     // Пины для управления скоростью через PWM
+#define ENA 6
 #define ENB 7
 
 // Пины для ультразвукового датчика
 #define TRIG_PIN 8
 #define ECHO_PIN 9
 
-Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN); // Инициализация ультразвукового датчика
+// Пины для сервомотора
+#define SERVO_PIN 10      // Управляющий пин для сервомотора
+#define SERVO_VCC_PIN 11  // Питание для сервомотора
+#define SERVO_GND_PIN 12  // GND для сервомотора
 
-// Время поворота
-#define TURN_TIME 500
+// Пины для датчиков цвета
+#define FRONT_COLOR_SENSOR_PIN 13 // Датчик спереди
+#define REAR_COLOR_SENSOR_PIN 14  // Датчик сзади
+
+Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN); // Инициализация ультразвукового датчика
+Servo servo;                               // Объект для управления сервомотором
+
+// Переменные для управления сервомотором
+unsigned long servoLastMoved = 0; // Время последнего поворота сервомотора
+const int servoDelay = 500;       // Задержка для стабилизации сервопривода
+bool servoTurned = false;         // Флаг для отслеживания состояния поворота сервомотора
+
+// Время движения при обнаружении черной линии
+const int moveTimeOnLine = 500;
 
 void setup() {
   // Устанавливаем пины для управления моторами как выходы
@@ -25,39 +41,78 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-  
+
+  // Настройка пинов питания для сервомотора
+  pinMode(SERVO_VCC_PIN, OUTPUT);
+  pinMode(SERVO_GND_PIN, OUTPUT);
+  digitalWrite(SERVO_VCC_PIN, HIGH);  // Подаем питание на серво
+  digitalWrite(SERVO_GND_PIN, LOW);   // Подключаем землю для серво
+
+  // Подключение сервопривода к управляющему пину
+  servo.attach(SERVO_PIN);
+
+  // Пины для датчиков цвета
+  pinMode(FRONT_COLOR_SENSOR_PIN, INPUT);
+  pinMode(REAR_COLOR_SENSOR_PIN, INPUT);
+
   // Инициализация скорости моторов
   analogWrite(ENA, 255); // Максимальная скорость для первого двигателя
   analogWrite(ENB, 255); // Максимальная скорость для второго двигателя
-  
+
   // Инициализация серийного монитора для отладки
   Serial.begin(9600);
 }
 
 void loop() {
   long distance = ultrasonic.read(); // Считываем расстояние с ультразвукового датчика
-  
-  // Проверяем, что расстояние меньше 255 (обнаружена цель)
-  if (distance < 255) {
-    moveForward(); // Если цель найдена, едем вперед
-    Serial.println("Цель найдена!");
-  } else {
-    // Если цель не найдена, начинаем крутиться
-    turnRight(TURN_TIME);
-    distance = ultrasonic.read(); // Переходим к следующей проверке
-    
-    if (distance >= 255) {
-      turnLeft(TURN_TIME); // Если после поворота вправо цель не найдена, поворачиваем влево
-      distance = ultrasonic.read(); // Считываем новое расстояние
-      
+
+  // Проверка черной линии спереди или сзади
+  if (digitalRead(FRONT_COLOR_SENSOR_PIN) == HIGH) {
+    // Если передний датчик обнаружил черную линию, отъезжаем назад
+    moveBackwardFor(moveTimeOnLine);
+  } 
+  else if (digitalRead(REAR_COLOR_SENSOR_PIN) == HIGH) {
+    // Если задний датчик обнаружил черную линию, едем вперед
+    moveForwardFor(moveTimeOnLine);
+  } 
+  else {
+    // Основная логика движения
+    if (distance < 5) {
+      // Если цель очень близко, проверяем состояние поворота сервомотора
+      if (!servoTurned) {
+        servo.write(60);                  // Поворачиваем сервомотор на 60 градусов
+        servoLastMoved = millis();        // Запоминаем время поворота
+        servoTurned = true;               // Устанавливаем флаг поворота
+      }
+    } else if (distance < 255) {
+      // Если цель найдена, но не слишком близко, едем вперед
+      moveForward();
+      Serial.println("Цель найдена!");
+    } else {
+      // Если цель не найдена, начинаем крутиться
+      turnRight(TURN_TIME);
+      distance = ultrasonic.read(); // Переходим к следующей проверке
+
       if (distance >= 255) {
-        // Если после поворота влево цель все равно не найдена, продолжаем крутиться влево
-        turnLeft(TURN_TIME);
+        turnLeft(TURN_TIME); // Если после поворота вправо цель не найдена, поворачиваем влево
+        distance = ultrasonic.read(); // Считываем новое расстояние
+
+        if (distance >= 255) {
+          // Если после поворота влево цель все равно не найдена, продолжаем крутиться влево
+          turnLeft(TURN_TIME);
+        }
       }
     }
   }
+
+  // Проверяем, прошло ли достаточно времени для стабилизации сервомотора
+  if (servoTurned && (millis() - servoLastMoved >= servoDelay)) {
+    servo.write(0);         // Возвращаем сервомотор в исходное положение
+    servoTurned = false;    // Сбрасываем флаг поворота
+  }
 }
 
+// Функция движения вперед
 void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
@@ -65,6 +120,15 @@ void moveForward() {
   digitalWrite(IN4, LOW);
 }
 
+// Функция движения назад
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+// Функция поворота влево
 void turnLeft(int duration) {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
@@ -73,10 +137,33 @@ void turnLeft(int duration) {
   delay(duration); // Время поворота
 }
 
+// Функция поворота вправо
 void turnRight(int duration) {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   delay(duration); // Время поворота
+}
+
+// Функция движения вперед на заданное время
+void moveForwardFor(int duration) {
+  moveForward();
+  delay(duration);
+  stopMotors();
+}
+
+// Функция движения назад на заданное время
+void moveBackwardFor(int duration) {
+  moveBackward();
+  delay(duration);
+  stopMotors();
+}
+
+// Функция остановки моторов
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
